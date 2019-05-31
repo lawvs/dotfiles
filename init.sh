21821bb1b3834ef590341fc8f7b92095ec36c6cb
lawvs <18554747+lawvs@users.noreply.github.com>
2019-06-01 03:48:44 +0800

    chore: update script

--- init.sh
+++ init.sh
@@ -11,17 +11,13 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
 apt update && apt upgrade
-apt install git vim build-essential screenfetch
-apt install curl nc
+apt install -y git vim build-essential screenfetch
+apt install -y curl netcat
 
 # install zsh
-sudo apt-get install zsh
+apt install -y zsh
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 chsh -s $(which zsh)
-
-# zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-set -e
-
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
 ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
 echo '[finished] vim'
 
+# zsh
 ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc
+# zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+chmod 0700 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+chmod 0700 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# theme
+wget "https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme" -O ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/bullet-train.zsh-theme
+
 echo '[finished] zsh'
 
