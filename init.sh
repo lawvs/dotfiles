8dfb46b13b1f24db7c32e647011d1305cab3bf6f
lawvs <18554747+lawvs@users.noreply.github.com>
2019-06-15 02:46:56 +0800

    fix: update

--- init.sh
+++ init.sh
@@ -10,14 +10,12 @@ fi
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
-apt update && apt upgrade
+apt update && apt upgrade -y
 apt install -y git vim build-essential screenfetch
 apt install -y curl netcat
 
 # install zsh
 apt install -y zsh
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 chsh -s $(which zsh)
 
--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ echo '[finished] vim'
 
 # zsh
 ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
