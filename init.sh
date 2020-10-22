41b1cfac49af5d0594604966555c10c675347ebe
lawvs <18554747+lawvs@users.noreply.github.com>
2020-10-22 22:51:36 +0900

    chore: update yarn

--- init.sh
+++ init.sh
@@ -75,9 +75,7 @@ install-nodejs() {
     echo "* Installing Yarn..."
     echo "-----------------------------------------------------------"
 
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt update && sudo apt install --no-install-recommends -y yarn
+    npm install -g yarn
 
     echo "-----------------------------------------------------------"
     echo -n "* Yarn Version: "
