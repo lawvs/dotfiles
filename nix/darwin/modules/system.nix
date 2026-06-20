9c539efa45cbd3d7926583acbcab34e093115346
lawvs <me@waterwater.moe>
2026-06-20 14:50:03 +0800

    feat: expand macos defaults

--- nix/darwin/modules/system.nix
+++ nix/darwin/modules/system.nix
@@ -3,18 +3,41 @@
   system.defaults = {
     NSGlobalDomain = {
       AppleShowAllExtensions = true;
+      AppleInterfaceStyleSwitchesAutomatically = true;
       ApplePressAndHoldEnabled = false;
       InitialKeyRepeat = 15;
       KeyRepeat = 2;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
     };
 
     finder = {
       AppleShowAllExtensions = true;
+      FXDefaultSearchScope = "SCcf";
+      FXEnableExtensionChangeWarning = false;
       FXPreferredViewStyle = "Nlsv";
       ShowPathbar = true;
       ShowStatusBar = true;
+      _FXSortFoldersFirst = true;
     };
 
     menuExtraClock.Show24Hour = true;
+
+    trackpad = {
+      Clicking = true;
+      TrackpadRightClick = true;
+      TrackpadThreeFingerDrag = true;
+    };
+
+    CustomUserPreferences = {
+      "com.apple.AdLib".allowApplePersonalizedAdvertising = false;
+
+      "com.apple.desktopservices" = {
+        DSDontWriteNetworkStores = true;
+        DSDontWriteUSBStores = true;
+      };
+    };
   };
 }
