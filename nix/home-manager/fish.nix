6eb05ee268416d8d50b2317f0ae633403c39fee7
lawvs <18554747+lawvs@users.noreply.github.com>
2026-07-02 23:02:55 +0800

    feat: add fish git abbreviations

--- nix/home-manager/fish.nix
+++ nix/home-manager/fish.nix
@@ -6,5 +6,17 @@
       ll = "ls -la";
       vim = "nvim";
     };
+
+    shellAbbrs = {
+      g = "git";
+      ga = "git add";
+      gb = "git branch";
+      gc = "git commit";
+      gco = "git checkout";
+      gl = "git pull";
+      gp = "git push";
+      gst = "git status";
+      gsw = "git switch";
+    };
   };
 }
